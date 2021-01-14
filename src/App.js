a28b81551e505eb7f1e37eacbda390b7658880a5
DragonMaster <ms235389@gmail.com>
2021-01-14 19:38:04 +0530

    initial-setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import {getStoryIds} from './services/hnApi'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+export const App = () => {
+    const [storyIds,
+        setStoryIds] = useState([])
+
+    useEffect(() => {
+        getStoryIds().then(data => setStoryIds(data))
+    }, [])
+
+    return (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {JSON.stringify(storyIds)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    )
 
-export default App;
+}
